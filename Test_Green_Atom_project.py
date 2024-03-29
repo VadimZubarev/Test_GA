b657301736ce44ae3b9a8fdbdd291b0b4c6b2209
VadimZubarev <vadim_zubarev_2003@mail.ru>
2024-03-29 20:53:27 +0300

    main

---
+++ Test_Green_Atom_project.py
@@ -0,0 +1,12 @@
+import asyncio
+import Robot
+
+i = 0
+
+async def main():
+    while(True):
+        task = asyncio.create_task(Robot())
+        return_value = await task
+        print(return_value)
+
+asyncio.run(main())
